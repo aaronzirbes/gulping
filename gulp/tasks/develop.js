e1a4811b2a10c4df72518e2514e2eab4a2c93e98
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-05-20 21:33:59 -0500

    asdf

--- gulp/tasks/develop.js
+++ gulp/tasks/develop.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 
 gulp.task('dev',function() {
+  gulp.start('build');
   watch(
     [ 'src/**/*.*', 'public/**/*.html' ],
     function() {
