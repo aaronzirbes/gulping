2e862b11bdb9b64f2c08093c4b48f8949fa115fb
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-05-20 22:50:32 -0500

    simpler

--- src/scripts/controllers/version-controller/version-controller.js
+++ src/scripts/controllers/version-controller/version-controller.js
@@ -1,7 +1,6 @@
 module.exports = function($scope, versionService) {
   versionService.getVersions()
     .then(function(response) {
-      console.log("versions: ", response.data);
       $scope.versions = response.data;
     });
 };
