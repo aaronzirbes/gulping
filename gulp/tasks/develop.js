cc9cc881e8e42f5857843704bab960c9d52d6959
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-05-20 22:52:25 -0500

    right?

--- gulp/tasks/develop.js
+++ gulp/tasks/develop.js
@@ -3,9 +3,7 @@ var watch = require('gulp-watch');
 
 gulp.task('dev',function() {
   gulp.start('build');
-  watch(
-    [ 'src/**/*.*', 'public/**/*.html' ],
-    function() {
+  watch([ 'src/**/*.*', 'public/**/*.html' ], function() {
       gulp.start('build');
     });
 });
